b4c24d942251ea906b9614927ef20d2bb5f1bf91
Romir Tandon <83617756+BottleOfBeans@users.noreply.github.com>
2022-01-12 23:12:52 -0500

    We do a little flasking, edited the main.py to try and get somehting working on this goddamn website.

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return "Hello, World!"
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/')
+def hello():
+    return 'Hello, World!'
