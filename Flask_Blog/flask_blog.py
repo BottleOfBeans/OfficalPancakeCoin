e661d1f39e4c3004b9faaaad52f6a159fe99f5b3
Nikitaoverlord <92127574+Nikitaoverlord@users.noreply.github.com>
2022-01-25 09:43:46 -0500

    Update flask_blog.py

--- Flask_Blog/flask_blog.py
+++ Flask_Blog/flask_blog.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def HomePage():
 	return render_template('index.html')
 
+@app.route('/users/<username>')
+def userPage(username):
+	return "Hello {username}!"
 
 @app.route('/about/')
 def AboutPage():
-	return HelloWorld!
+	return "HelloWorld!"
 
